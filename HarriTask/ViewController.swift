6d276e3ce3fc403747ef7fdf98960d057f80aac9
Amr AbuZant <amrabuzant@gmail.com>
2019-03-05 12:27:22 +0200

    Initial Commit

---
+++ HarriTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarriTask
+//
+//  Created by Amr Abu Zant on 3/5/19.
+//  Copyright © 2019 Amr Abu Zant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
